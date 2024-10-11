2913a9b3c0f6c866cbae69a5919fb4c7aebe513a
Salem Yaslem <s@sy.sa>
2024-10-11 11:21:25 +0300

    translate

--- src/App.tsx
+++ src/App.tsx
@@ -121,7 +121,7 @@ function App() {
                 onClick={resetGame}
                 className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
               >
-                بدء لعبة جديدة
+                لعبة جديدة
               </button>
             </div>
             <GameBoard board={board} placeBlock={placeBlock} />
